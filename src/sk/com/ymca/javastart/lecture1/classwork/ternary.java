46d39f150c59c4d8509f936ed551a8a1013bb726
Eugene <kladberovv@gmail.com>
2024-05-01 12:33:19 +0200

    classwork

--- src/sk/com/ymca/javastart/lecture1/classwork/ternary.java
+++ src/sk/com/ymca/javastart/lecture1/classwork/ternary.java
@@ -3,7 +3,10 @@ package sk.com.ymca.javastart.lecture1.classwork;
 public class ternary {
     public static void main(String[] args) {
         int a = 10;
-        int cost = 20;
+        int b = 20;
+        int max;
 
+        max = (a > b) ? a : b;
+        System.out.println(max);
     }
 }
