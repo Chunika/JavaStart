e0320ab8f682a78d17617a4903c53790908cab6c
Eugene <kladberovv@gmail.com>
2024-05-10 16:02:34 +0200

    classwork

--- src/sk/com/ymca/javastart/lecture1/calculator.java
+++ src/sk/com/ymca/javastart/lecture1/calculator.java
@@ -5,39 +5,33 @@ public class calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введіть значення operand1: ");
+        System.out.print("Enter the value of operand1: ");
         double operand1 = scanner.nextDouble();
 
-        System.out.print("Введіть значення operand2: ");
+        System.out.print("Enter the value of operand2: ");
         double operand2 = scanner.nextDouble();
 
-        System.out.print("Введіть знак арифметичної операції (+, -, *, /): ");
+        System.out.print("Enter the arithmetic operation sign (+, -, *, /): ");
         String sign = scanner.next();
-
-        double result;
         switch (sign) {
             case "+":
-                result = operand1 + operand2;
-                System.out.println("Результат: " + result);
+                System.out.println("Result: " + (operand1 + operand2));
                 break;
             case "-":
-                result = operand1 - operand2;
-                System.out.println("Результат: " + result);
+                System.out.println("Result: " + (operand1 - operand2));
                 break;
             case "*":
-                result = operand1 * operand2;
-                System.out.println("Результат: " + result);
+                System.out.println("Result: "+ (operand1 * operand2));
                 break;
             case "/":
                 if (operand2 == 0) {
-                    System.out.println("Ділення на 0 неможливе!!");
+                    System.out.println("Division by 0 is not possible!!!");
                 } else {
-                    result = operand1 / operand2;
-                    System.out.println("Результат: " + result);
+                    System.out.println("Result: "+  (operand1 / operand2));
                 }
                 break;
             default:
-                System.out.println("Помилка, невідомий знак!!!");
+                System.out.println("Error, unknown sign!!!");
         }
     }
 }
